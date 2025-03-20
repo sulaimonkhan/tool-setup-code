797df1bb48ea8e3d67b54f94c8ca7fde1c0b6e60
sulaimonkhan <sulaimonkhan04@gmail.com>
2025-03-20 15:37:29 +0530

    vault

---
+++ misc/vault_secrets/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-d86"
+    key    = "vault-secrets/terraform.tfstate"
+    region = "us-east-1"
+  }    
+}
+
+provider "vault" {
+  address = "http://vault-internal.sulaimondevopsb72.online:8200"
+  token   = var.vault_token
+  skip_tls_verify = true    
+}
+
+variable "vault_token" {}
+
+resource "vault_generic_secret" "roboshop-dev" {
+  path = "roboshop-dev/frontend"
+
+  data_json = <<EOT    
+}
+  "foo":    "bar",
+  "pizza":  "cheese"
+
+{
+  EOT    
+}  
